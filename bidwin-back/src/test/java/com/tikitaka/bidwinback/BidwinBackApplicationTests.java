2ef7a5968be0cb48c7bd25e62e6df9c9e746d333
Rhoo-se <tmddjr_0331@naver.com>
2026-07-23 14:08:24 +0900

    chore: 백엔드 초기 세팅 (#0)

---
+++ bidwin-back/src/test/java/com/tikitaka/bidwinback/BidwinBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tikitaka.bidwinback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidwinBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
