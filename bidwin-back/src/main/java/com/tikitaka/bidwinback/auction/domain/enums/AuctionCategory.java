d95c264bba41ee98725f183dd6ab8f0a1e7850d7
CHANUK HEO <siai3647@gmail.com>
2026-07-24 19:05:23 +0900

    feat: 도메인 상태 enum 정의 (#3)

---
+++ bidwin-back/src/main/java/com/tikitaka/bidwinback/auction/domain/enums/AuctionCategory.java
@@ -0,0 +1,7 @@
+package com.tikitaka.bidwinback.auction.domain.enums;
+
+public enum AuctionCategory {
+    HOUSEHOLD_GOODS,
+    FOOD_SHARING,
+    FURNITURE
+}
---
+++ bidwin-back/src/main/java/com/tikitaka/bidwinback/auction/domain/enums/AuctionStatus.java
@@ -0,0 +1,10 @@
+package com.tikitaka.bidwinback.auction.domain.enums;
+
+public enum AuctionStatus {
+    OPEN,
+    BID_ONGOING,
+    WINNER_DETERMINING,
+    COMPLETED,
+    UNSOLD,
+    CANCELED
+}
