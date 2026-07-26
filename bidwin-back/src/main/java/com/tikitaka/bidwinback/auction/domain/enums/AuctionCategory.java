9e1dccb106263f3dc9b6bec8133024772260607b
Rhoo-se <tmddjr_0331@naver.com>
2026-07-26 17:00:48 +0900

    feat: 카테고리 초기 데이터 등록 (#11)

--- bidwin-back/src/main/java/com/tikitaka/bidwinback/auction/domain/enums/AuctionCategory.java
+++ bidwin-back/src/main/java/com/tikitaka/bidwinback/auction/domain/enums/AuctionCategory.java
@@ -1,7 +1,17 @@
 package com.tikitaka.bidwinback.auction.domain.enums;
 
-public enum AuctionCategory {
-    HOUSEHOLD_GOODS,
-    FOOD_SHARING,
-    FURNITURE
+public enum AuctionCategory{
+    HOUSEHOLD("생활용품"),
+    FOOD("먹거리"),
+    FURNITURE("가구");
+
+    private final String label;
+
+    AuctionCategory(String label){
+        this.label = label;
+    }
+
+    public String getLabel(){
+        return this.label;
+    }
 }
