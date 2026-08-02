3ec73b97c59734d7973a73d16f466922139e5a5d
kim_geunseong <rootachieve6053@gmail.com>
2026-08-02 23:10:43 +0900

    feat: 입찰에 상태 컬럼 추가 (#45)
    Co-Authored-By: CHANUK HEO <siai3647@gmail.com>
    Co-Authored-By: Rhoo-se <108392388+Rhoo-se@users.noreply.github.com>

--- bidwin-back/src/main/java/com/tikitaka/bidwinback/auction/domain/entity/Bid.java
+++ bidwin-back/src/main/java/com/tikitaka/bidwinback/auction/domain/entity/Bid.java
@@ -1,9 +1,12 @@
 package com.tikitaka.bidwinback.auction.domain.entity;
 
+import com.tikitaka.bidwinback.auction.domain.enums.BidStatus;
 import com.tikitaka.bidwinback.global.common.entity.BaseTimeEntity;
 import com.tikitaka.bidwinback.member.domain.entity.Member;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -39,10 +42,15 @@ public class Bid extends BaseTimeEntity {
     @Column(nullable = false)
     private long price;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private BidStatus status;
+
     @Builder
-    private Bid(Auction auction, Member bidder, long price) {
+    private Bid(Auction auction, Member bidder, long price, BidStatus status) {
         this.auction = auction;
         this.bidder = bidder;
         this.price = price;
+        this.status = status;
     }
 }
