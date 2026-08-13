a58a6e132e2c003ef34af030bb247732ab667448
CHANUK HEO <siai3647@gmail.com>
2026-08-13 14:03:52 +0900

    refactor: 경매 스냅샷 인덱스 마이그레이션 통합 (#182)

--- bidwin-back/src/main/resources/db/migration/V6__optimize_auction_snapshot_price_index.sql
+++ bidwin-back/src/main/resources/db/migration/V6__optimize_auction_snapshot_price_index.sql
@@ -1,3 +1,9 @@
+ALTER TABLE auction
+    MODIFY COLUMN auction_type VARCHAR(4)
+        CHARACTER SET ascii
+        COLLATE ascii_bin
+        NOT NULL;
+
 CREATE INDEX idx_auction_snapshot_price
     ON auction (
         auction_type,
--- bidwin-back/src/main/resources/db/migration/V6__shrink_auction_type.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE auction
-    MODIFY COLUMN auction_type VARCHAR(4)
-        CHARACTER SET ascii
-        COLLATE ascii_bin
-        NOT NULL;
